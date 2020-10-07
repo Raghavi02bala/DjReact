35e997d2894c7f23dcf7ef4316b02247a4a421f7
Raghavi02bala <raghavibalasubramania@gmail.com>
2020-10-07 11:54:02 +0530

    done

--- gui/src/components/Form.js
+++ gui/src/components/Form.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import { Form, Input, Button } from 'antd';
 
-const CustomForm = () => {
-    
-    function  handleFormSubmit(event) {
+function CustomForm() {
+    function handleFormSubmit(event) {
         event.preventDefault();
         const title = event.target.elements.title.value;
         const content = event.target.elements.content.value;
@@ -11,21 +10,22 @@ const CustomForm = () => {
         console.log(title, content);
     }
 
+
     return (
         <>
-        <Form onSubmit={handleFormSubmit}>
-            <Form.Item label="Title">
-            <Input name="title" placeholder="Put title here" />
-            </Form.Item>
-            <Form.Item label="Content">
-            <Input name="content" placeholder="Put Content here..." />
-            </Form.Item>
-            <Form.Item >
-            <Button type="primary" htmlType="submit">Submit</Button>
-            </Form.Item>
-        </Form>
+            <Form onSubmit={handleFormSubmit}>
+                <Form.Item label="Title">
+                    <Input name="title" placeholder="Put title here" />
+                </Form.Item>
+                <Form.Item label="Content">
+                    <Input name="content" placeholder="Put Content here..." />
+                </Form.Item>
+                <Form.Item >
+                    <Button type="primary" htmlType="submit">Submit</Button>
+                </Form.Item>
+            </Form>
         </>
     );
-    };
+}
 
 export default CustomForm;
